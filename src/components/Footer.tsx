47ccee67eaaacb2d60bee1d11a85fd694b22454d
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-08-21 10:11:03 -0400

    Styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer: React.FC = () => {
   return (
     <footer className="select-none px-6 py-4 pt-8 text-center text-neutral-500">
-      &copy; {new Date().getFullYear()} Kirill Tregubov. This page is{' '}
+      &copy; {new Date().getFullYear()} Kirill Tregubov. This site is{' '}
       <a
         rel="noopener noreferrer"
         href="https://github.com/KirillTregubov/kirilltregubov.com"
         target="_blank"
-        className="underline underline-offset-1"
+        className="underline underline-offset-2"
       >
         open source
       </a>
