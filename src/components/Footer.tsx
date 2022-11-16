27eccd7d6d475b421cd45a598948988321a19ade
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-11-16 03:01:14 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="select-none px-6 py-4 pt-8 text-center text-neutral-400">
+    <footer className="select-none px-2 py-4 pt-8 text-center text-neutral-400">
       &copy; {new Date().getFullYear()} Kirill Tregubov. This site is{' '}
       <a
         rel="noopener noreferrer"
