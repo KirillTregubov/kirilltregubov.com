280e0c8105fc94e3f6c8304e0359bbd2338ef38c
Kirill Tregubov <contact@kirilltregubov.com>
2024-05-29 02:42:20 -0400

    fixes

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -9,7 +9,6 @@ let tabs = [
 
 export default function Tabs() {
   const [activeTab, setActiveTab] = useState(tabs[0].id)
-  const [isFocused, setIsFocused] = useState(false)
 
   return (
     <div className="flex space-x-2">
@@ -17,13 +16,9 @@ export default function Tabs() {
         <button
           key={tab.id}
           onClick={() => setActiveTab(tab.id)}
-          onFocus={() => {
-            if (activeTab === tab.id) setIsFocused(true)
-          }}
-          onBlur={() => setIsFocused(false)}
           className={`${
             activeTab === tab.id
-              ? '!ring-0'
+              ? 'ring-offset-1 ring-offset-neutral-900'
               : 'hover:text-neutral-400 focus-visible:text-neutral-400'
           } group relative !rounded-full px-3 py-1.5 font-medium transition-[color,box-shadow]`}
           style={{
@@ -35,7 +30,6 @@ export default function Tabs() {
               layoutId="bubble"
               className="absolute inset-0 z-10 bg-white mix-blend-difference transition-colors group-focus-visible:bg-neutral-200"
               style={{ borderRadius: '9999px' }}
-              animate={{ inset: isFocused ? '-0.125rem' : 0 }}
               transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
             />
           )}
