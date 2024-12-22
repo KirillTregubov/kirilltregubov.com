8fe0fc3be258a02a9a613ee1f06a9cdf3da01c9a
Kirill Tregubov <contact@kirilltregubov.com>
2024-12-22 16:15:56 -0500

    make it a little nicer and only render during holidays

---
+++ src/components/Snow.tsx
@@ -0,0 +1,16 @@
+import Snowfall from 'react-snowfall'
+
+const snowflake1 = document.createElement('img')
+snowflake1.src = '/assets/snowflake.png'
+
+export default function Snow() {
+  return (
+    <Snowfall
+      changeFrequency={400}
+      opacity={[0.3, 0.5]}
+      radius={[0.5, 10.0]}
+      rotationSpeed={[-0.5, 0.5]}
+      images={[snowflake1]}
+    />
+  )
+}
