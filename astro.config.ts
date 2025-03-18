2e1fe886f1fd892dd0a504f24b60d30fd6add0eb
Kirill Tregubov <contact@kirilltregubov.com>
2025-03-18 18:31:38 -0400

    add owcs juno

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
-
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
