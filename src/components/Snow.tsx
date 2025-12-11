e2e4261fd5a6340ff1d471dff2f009c15103e4c7
Kirill Tregubov <contact@kirilltregubov.com>
2025-12-11 00:05:15 -0500

    feat: add new backgrounds

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -1,9 +1,25 @@
+import { useMemo } from 'react'
 import Snowfall from 'react-snowfall'
+import useIsMobile from './hooks/useIsMobile'
 
 const snowflake = document.createElement('img')
 snowflake.src = '/assets/snowflake.png'
 
 export default function Snow() {
+  const isHolidaySeason = useMemo(() => {
+    const today = new Date()
+    return (
+      today.getMonth() === 11 ||
+      (today.getMonth() === 0 && today.getDate() <= 5)
+    )
+  }, [])
+
+  if (!isHolidaySeason) {
+    return null
+  }
+
+  const { isMobile } = useIsMobile()
+
   return (
     <Snowfall
       changeFrequency={400}
@@ -13,6 +29,7 @@ export default function Snow() {
       rotationSpeed={[-0.5, 0.5]}
       images={[snowflake]}
       style={{ zIndex: 999 }}
+      snowflakeCount={isMobile ? 150 : 300}
     />
   )
 }
